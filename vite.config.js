8b0906348bcf802875674a2eaeff5d511f6ac5a3
Yaron Hayardeni <yaronHayar@gmail.com>
2022-06-24 18:24:10 +0300

    Another Fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    server: {
-        port: 9001
-    },
-    build: {
-        outDir: './docs' // for github pages
-    }
+	server: {
+		port: 9001
+	},
+	build: {
+		outDir: './docs', // for github pages
+	},
+    base: '/WoozyGraph/',
 })
